22918d9649383171b9cb590d4b115d17c26422f7
makirby <makirby@outlook.com>
2015-05-20 14:28:54 +0100

    Model changes, added more database seeding on init. Database now persists

--- src/main/java/ssd/comedyvenue/setup/Database.java
+++ src/main/java/ssd/comedyvenue/setup/Database.java
@@ -1,5 +1,6 @@
 package ssd.comedyvenue.setup;
 
+import com.j256.ormlite.support.ConnectionSource;
 import com.j256.ormlite.table.TableUtils;
 import org.joda.time.DateTime;
 import ssd.comedyvenue.model.*;
@@ -12,26 +13,27 @@ public class Database {
     Repository<Customer> customerRepo = new CustomerRepository();
     Repository<Event> eventRepo = new EventRepository();
     Repository<Booking> bookingRepo = new BookingRepository();
+    Repository<Feedback> feedbackRepo = new FeedbackRepository();
 
     /* Creates database tables and seeds with a set of information */
     public void init(){
 
         try {
+
+            if(null != ConnectionProvider.getConnection().getDatabaseType()){
+                return;
+            }
+
             //setup database tables
             createTables();
-
             //seed customers
             createCustomers();
-
             //seed comedians
             createComedians();
-
             //seed events
             createEvents();
-
             //seed bookings
             createBookings();
-
             //seed feedback
             createFeedback();
 
@@ -40,6 +42,7 @@ public class Database {
         }
     }
 
+    //creates the database tables
     private void createTables() throws Exception{
         //Create tables in DB
         TableUtils.createTableIfNotExists(ConnectionProvider.getConnection(), Booking.class);
@@ -49,6 +52,7 @@ public class Database {
         TableUtils.createTableIfNotExists(ConnectionProvider.getConnection(), Feedback.class);
     }
 
+    //creates a selection of customer entitites and inserts into the database
     private void createCustomers(){
 
         DateTime now = DateTime.now();
@@ -58,7 +62,6 @@ public class Database {
         Customer customer4 = new Customer("Jack Charlton", now.minusYears(62).toDate() ,"084246375719");
         Customer customer5 = new Customer("Peter Shilton", now.minusYears(29).toDate() ,"081646278719");
 
-
         customerRepo.add(customer1);
         customerRepo.add(customer2);
         customerRepo.add(customer3);
@@ -67,6 +70,7 @@ public class Database {
 
     }
 
+    //creates a selection of comedian entities in the database
     private void createComedians(){
 
         Comedian comedian1 = new Comedian("Jimbo Jones", "Jim The Clown", "07239237421");
@@ -75,15 +79,14 @@ public class Database {
         Comedian comedian4 = new Comedian("Ian Witten", "Jack Dexter", "07239237421");
         Comedian comedian5 = new Comedian("Dean Mumm", "Madame Chang", "07239237421");
 
-
         comedianRepo.add(comedian1);
         comedianRepo.add(comedian2);
         comedianRepo.add(comedian3);
         comedianRepo.add(comedian4);
         comedianRepo.add(comedian5);
-
     }
 
+    //creates a selection of events in the database
     private void createEvents(){
 
         DateTime now = DateTime.now();
@@ -99,27 +102,35 @@ public class Database {
         eventRepo.add(event2);
         eventRepo.add(event3);
         eventRepo.add(event4);
-
     }
 
+    //creates a selection of bookings in the database
     private void createBookings(){
 
-        Customer customer1 = customerRepo.list().get(3);
+        Customer customer1 = customerRepo.list().get(2);
+        Customer customer2 = customerRepo.list().get(3);
         Event event1 = eventRepo.list().get(1);
         Event event2 = eventRepo.list().get(3);
 
         Booking booking1 = new Booking(customer1, 1, event1);
         Booking booking2 = new Booking(customer1, 3, event2);
+        Booking booking3 = new Booking(customer2, 2, event2);
 
         bookingRepo.add(booking1);
         bookingRepo.add(booking2);
-
+        bookingRepo.add(booking3);
     }
 
+    //creates a selection of feedback in the database
     private void createFeedback(){
 
+        Booking booking1 = bookingRepo.getById(2);
+        Booking booking2 = bookingRepo.getById(3);
 
+        Feedback feedback1 = new Feedback(booking1, "Wow what a great show", 4);
+        Feedback feedback2 = new Feedback(booking2, "It was alright", 3);
 
-
+        feedbackRepo.add(feedback1);
+        feedbackRepo.add(feedback2);
     }
 }
